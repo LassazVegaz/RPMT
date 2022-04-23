8d8a8883989ccbf4f49ad8cc761af34f82426b3d
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-23 16:34:44 +0530

    header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import { Typography, Container } from "@mui/material";
+import { Header } from "./components/Header/Header";
 
 function App() {
-	return (
-		<Container maxWidth="xl">
-			<Typography variant="h1">Hello World</Typography>
-		</Container>
-	);
+	return <Header />;
 }
 
 export default App;
