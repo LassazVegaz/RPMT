4aa788a6628dd56fab7c7b5570b26a4d9ab93730
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-23 16:55:42 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { Typography } from "@mui/material";
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 
 function App() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+
+			<Typography mt={30} mb={33} variant="h1">
+				Large content
+			</Typography>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
