1e483a30161c01d69d9548947e11821a7aedf68e
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-30 00:12:29 +0530

    model: better student group relationship

--- backend/models/group.model.js
+++ backend/models/group.model.js
@@ -1,22 +1,18 @@
 import mongoose from "mongoose";
 
 const _schema = mongoose.Schema({
-  _id: false,
-  groupId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
+	name: {
+		type: String,
+		required: true,
+	},
 });
 
-_schema.virtual("student", {
-  ref: "student",
-  localField: "studentId",
-  foreignField: "userId",
-});
+_schema.virtual("students", [
+	{
+		ref: "Student",
+		localField: _id,
+		foreignField: "userId",
+	},
+]);
 
 export const Supervisor = mongoose.model("Group", _schema);
