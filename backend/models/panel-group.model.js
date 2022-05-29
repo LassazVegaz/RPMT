a8004ef81125c0a96a9107d4799c8da3eb52b013
Chathura Madhusanka <cmadhusanka29@gmail.com>
2022-05-29 22:34:02 +0530

    panel member - group schema

---
+++ backend/models/panel-group.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const _schema = mongoose.Schema({
+	groupId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	staffMemberId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+});
+
+_schema.virtual("staffMember", {
+	ref: "staffMember",
+	localField: "staffMemberId",
+});
+
+_schema.virtual("Group", {
+	ref: "Group",
+	localField: "GroupID",
+});
+
+export const ProjectSupervisor = mongoose.model("PanelMemberGroup", _schema);
