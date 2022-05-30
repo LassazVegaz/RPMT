0474e7cbb22e3222584af61d5099b9022ea91680
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-30 18:03:27 +0530

    staff member services

--- backend/services/staff-members.service.js
+++ backend/services/staff-members.service.js
@@ -12,9 +12,43 @@ const createStaffMember = async (user, staffMember) => {
 
 	const _staffMember = new StaffMember(staffMember);
 	await _staffMember.save();
-	return _staffMember.toJSON();
+
+	return getStaffMember(_staffMember.id);
+};
+
+const deleteStaffMember = async (id) => {
+	await StaffMember.findByIdAndDelete(id);
+};
+
+const updateStaffMember = async (id, staffMember) => {
+	const existingMember = await StaffMember.findById(id);
+
+	if (staffMember.firstName) existingMember.firstName = staffMember.firstName;
+	if (staffMember.lastName) existingMember.lastName = staffMember.lastName;
+	if (staffMember.phone) existingMember.phone = staffMember.phone;
+
+	await existingMember.save();
+
+	return getStaffMember(id);
+};
+
+const updatePhoto = async (id, photo) => {
+	const existingMember = await StaffMember.findById(id);
+	existingMember.photo = photo;
+	await existingMember.save();
+
+	return getStaffMember(id);
+};
+
+const getStaffMember = async (id) => {
+	const staffMember = await StaffMember.findById(id).populate("user");
+	return staffMember.toJSON();
 };
 
 export const staffMembersService = {
 	createStaffMember,
+	deleteStaffMember,
+	getStaffMember,
+	updateStaffMember,
+	updatePhoto,
 };
