5e86bf1a98ad2332244919c5409692b8330e4442
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-31 19:27:04 +0530

    init routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { SignupPage } from "./pages/SignupPage/SignupPage";
 
-import { Home } from "./pages/StudentHomePage/StudentHomePage";
-import { CreateGroups } from "./pages/CreateGroupPage/CreateGroupPage";
-import { Registertopic } from "./pages/RegisterTopicPage/RegisterTopicPage";
-import { ViewSupervisorFeedbackPage } from "./pages/ViewSupervisorFeedbackPage/ViewSupervisorFeedbackPage";
-import { Requests } from "./pages/RequestsPage/RequestsPage";
-import { SubmitDocumentsPage } from "./pages/SubmitDocumentsPage/SubmitDocumentsPage";
-import { DownloadTemplatePage } from "./pages/DownloadTemplatePage/DownloadTemplatePage";
-import { AdminHomePage } from "./pages/AdminHomePage/AdminHomePage";
-import {AddPanelMemberPage} from "./pages/AddPanleMember/AddPanelMemberPage";
-import {CreateMarkingScheme} from "./pages/CreateMarkingScheme/CreateMarkingScheme";
-import {ViewProfilesPage} from "./pages/ViewProfilesPage/ViewProfilesPage";
-import { UploadDocumentPage } from "./pages/UploadDocumentPage/UploadDocumentPage";
-import { AllocatePanelMembersPage } from "./pages/AllocatePnaleMembersPage/AllocatePanelMembersPage";
-import { ViewAddedPanelMembers } from "./pages/ViewAddedPanelMembers/ViewAddedPanelMembers";
-import { GroupDetailsPage } from "./pages/GroupDetailsPage/GroupDetailsPage";
-import { CreateSubmissionTypePage } from "./pages/CreateSubmissionTypePage/CreateSubmissionTypePage";
 function App() {
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header />
 
-      <Home />
-      <CreateGroups />
-      <Registertopic />
-      <ViewSupervisorFeedbackPage isCoSupervisor={true} />
-      <Requests />
-      <SubmitDocumentsPage />
-      <DownloadTemplatePage />
-      <AdminHomePage/>
-      <AddPanelMemberPage/>
-      <CreateMarkingScheme/>
-      <ViewProfilesPage/>
-      <UploadDocumentPage/>
-      <AllocatePanelMembersPage/>
-      <ViewAddedPanelMembers/>
-      <GroupDetailsPage/>
-      <CreateSubmissionTypePage/>
-      <Footer />
-    </>
-  );
+			<Routes>
+				<Route path="/" element={<SignupPage />} />
+			</Routes>
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { theme } from "./theme";
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
