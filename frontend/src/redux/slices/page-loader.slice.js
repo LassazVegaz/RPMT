941942f91a3884868e94582dd344ee9e7ca26865
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-31 20:25:40 +0530

    page loader slice

---
+++ frontend/src/redux/slices/page-loader.slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	isLoading: false,
+};
+
+export const pageLoaderSlice = createSlice({
+	name: "pageLoader",
+	initialState,
+	reducers: {
+		startLoading: (state) => (state.isLoading = true),
+		stopLoading: (state) => (state.isLoading = false),
+	},
+});
+
+export const pageLoaderActions = pageLoaderSlice.actions;
--- frontend/src/redux/state.js
+++ frontend/src/redux/state.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { pageLoaderSlice } from "./slices/page-loader.slice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		pageLoader: pageLoaderSlice.reducer,
+	},
 });
