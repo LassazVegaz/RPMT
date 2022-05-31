722d9b7b20cb1d5dd8b28d6eed3430b30b664b36
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-05-31 19:37:17 +0530

    store init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,24 @@
-import { ThemeProvider } from "@mui/material";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { theme } from "./theme";
-import "./index.scss";
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { theme } from "./theme";
+import { store } from "./redux/state";
+import App from "./App";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>
 );
