8ee4fd5babab3b560cf46250b4bc102433421f9c
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-02 01:09:52 +0530

    authentication & authorization

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -4,8 +4,7 @@ export const auth =
 	(roles = []) =>
 	async (req, res, next) => {
 		try {
-			const token =
-				req.headers["x-access-token"] || req.headers["authorization"];
+			const token = req.headers["authorization"]?.split(" ")[1];
 			if (!token)
 				return res
 					.status(401)
