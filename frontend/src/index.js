b15197b42c7698c3aa79d80008479eb636104906
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-01 10:00:49 +0530

    strict mode removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,13 +12,11 @@ import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<ThemeProvider theme={theme}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</ThemeProvider>
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
+	</Provider>
 );
