4be7805ce96e9baa8395e448d125e12095983ef8
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-02 09:37:53 +0530

    axios app update for auth

--- frontend/src/helpers/axios-app.js
+++ frontend/src/helpers/axios-app.js
@@ -1,10 +1,23 @@
 import axios from "axios";
+import { authHelper } from "./auth.helper";
 import config from "../config.json";
 
 const createAxiosApp = () => {
 	const _axios = axios.create({
 		baseURL: config.apiURL,
 	});
+
+	// add auth token to request header
+	_axios.interceptors.request.use(
+		(config) => {
+			const token = authHelper.getToken();
+			if (token) {
+				config.headers.Authorization = `Bearer ${token}`;
+			}
+			return config;
+		},
+		(error) => Promise.reject(error)
+	);
 
 	_axios.interceptors.response.use(null, (error) => {
 		console.error(error);
