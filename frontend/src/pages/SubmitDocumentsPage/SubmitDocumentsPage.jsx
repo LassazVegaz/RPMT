2706d293e10d8e84112e865573c0d20c64a39d72
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-03 23:04:46 +0530

    file format

--- frontend/src/pages/SubmitDocumentsPage/SubmitDocumentsPage.jsx
+++ frontend/src/pages/SubmitDocumentsPage/SubmitDocumentsPage.jsx
@@ -3,94 +3,82 @@ import { Button, Container, Paper, Typography, Box } from "@mui/material";
 import { FileUpload } from "../../components/FileUpload/FileUpload";
 import * as Yup from "yup";
 import { useFormik } from "formik";
-import { FormikMUITextField } from "../../components/FormikMUITextField/FormikMUITextField";
 import { useSubmissions } from "../../hooks/submit-document.hook";
 import { useNavigate } from "react-router-dom";
 
 const validationSchema = Yup.object({
-  name: Yup.string().required("Name is required"),
+	name: Yup.string().required("Name is required"),
 });
 
 const initialValues = {
-  name: "",
+	name: "",
 };
 
 export const SubmitDocuments = () => {
-  const navigate = useNavigate();
-  const { createSumission } = useSubmissions();
-  const form = useFormik({
-    validationSchema,
-    initialValues,
-    onSubmit: async (values) => {
-      console.log(values);
-      await createSumission({
-        name: values.name,
-      });
-      navigate("/download-templates");
-    },
-  });
+	const navigate = useNavigate();
+	const { createSumission } = useSubmissions();
+	const form = useFormik({
+		validationSchema,
+		initialValues,
+		onSubmit: async (values) => {
+			console.log(values);
+			await createSumission({
+				name: values.name,
+			});
+			navigate("/download-templates");
+		},
+	});
 
-  return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        my: 10,
-      }}
-    >
-      <Typography variant="h4" mb={10} textAlign="center" fontFamily={"areal"}>
-        Submit the Document ..
-      </Typography>
+	return (
+		<Container
+			maxWidth="lg"
+			sx={{
+				my: 10,
+			}}
+		>
+			<Typography
+				variant="h4"
+				mb={10}
+				textAlign="center"
+				fontFamily={"areal"}
+			>
+				Submit the Document Topic Document
+			</Typography>
 
-      <Paper
-        elevation={8}
-        sx={{
-          py: 3,
-          mx: 20,
-        }}
-      >
-        <Box
-          onSubmit={form.handleSubmit}
-          component="form"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            mx: 4,
-            rowGap: 4,
-          }}
-        >
-          <br />
+			<Paper
+				elevation={8}
+				sx={{
+					py: 3,
+					mx: 20,
+				}}
+			>
+				<Box
+					onSubmit={form.handleSubmit}
+					component="form"
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						mx: 4,
+						rowGap: 4,
+					}}
+				>
+					<FileUpload />
 
-          <Typography variant="h6" textAlign="left" fontFamily={"areal"}>
-            Submission Type :
-          </Typography>
-
-          <FormikMUITextField
-            name="name"
-            label="Enter Submission Type"
-            form={form}
-          />
-
-          <Typography variant="h6" textAlign="left" fontFamily={"areal"}>
-            Submit the document :
-          </Typography>
-
-          <FileUpload />
-
-          <Box
-            sx={{
-              my: 4,
-              px: 30,
-              display: "flex",
-              flexDirection: "column",
-              rowGap: 2,
-            }}
-          >
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </Box>
-        </Box>
-      </Paper>
-    </Container>
-  );
+					<Box
+						sx={{
+							my: 4,
+							px: 30,
+							display: "flex",
+							flexDirection: "column",
+							rowGap: 2,
+						}}
+					>
+						<Button variant="contained" type="submit">
+							Submit
+						</Button>
+					</Box>
+				</Box>
+			</Paper>
+		</Container>
+	);
 };
