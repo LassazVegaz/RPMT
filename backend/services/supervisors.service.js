2b730c5842342f6785d516f74917f6f50a819ca0
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-03 09:58:29 +0530

    sup serivce: send group with project fixes

--- backend/services/supervisors.service.js
+++ backend/services/supervisors.service.js
@@ -102,7 +102,7 @@ const _getProjects = async (supervisorId, status) => {
 				"coSupervisorId.status": status,
 			},
 		],
-	}).populate("Group");
+	}).populate("group");
 };
 
 const getProjects = async (supervisorId, status = "all") => {
