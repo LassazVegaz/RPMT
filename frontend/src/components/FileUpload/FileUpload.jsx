ba1c6bf1275adf3749d38c3b6b90ddfb82013bbe
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-03 23:58:37 +0530

    common file uploader

--- frontend/src/components/FileUpload/FileUpload.jsx
+++ frontend/src/components/FileUpload/FileUpload.jsx
@@ -1,30 +1,63 @@
-import {Typography, IconButton, Box} from "@mui/material"
-import {Upload as UploadIcon, Close as CloseIcon} from "@mui/icons-material"
+import { Typography, IconButton, Box } from "@mui/material";
+import { Upload as UploadIcon, Close as CloseIcon } from "@mui/icons-material";
+import { useState, useRef } from "react";
 
+export const FileUpload = ({ onChange }) => {
+	const [photo, setPhoto] = useState(null);
+	const fileInputRef = useRef(null);
 
-export const FileUpload = () => {
-  return (
-    <Box
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    mt={5}
-    flexDirection="column"
-  >
-    <Box         
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    >
-      <Typography variant="body1"> Sample.pdf</Typography>
-      <IconButton>
-          <CloseIcon/>
-      </IconButton>
-    </Box>
-    <IconButton>
-      <UploadIcon/>
-    </IconButton>
-  </Box>
-  )
-}
+	const handlePhotoChange = (event) => {
+		if (event.target.files.length > 0) {
+			const reader = new FileReader();
+			const fileExtension = event.target.files[0].name.split(".").pop();
 
+			reader.onload = (e) => {
+				const photo = {
+					data: e.target.result,
+					fileExtension,
+					fileName: event.target.files[0].name,
+				};
+
+				setPhoto(photo);
+				onChange(photo);
+			};
+			reader.readAsDataURL(event.target.files[0]);
+		}
+	};
+
+	return (
+		<Box
+			display="flex"
+			justifyContent="center"
+			alignItems="center"
+			mt={5}
+			flexDirection="column"
+		>
+			{photo?.fileName && (
+				<Box display="flex" justifyContent="center" alignItems="center">
+					<Typography variant="body1">{photo.fileName}</Typography>
+					<IconButton onClick={() => setPhoto(null)}>
+						<CloseIcon />
+					</IconButton>
+				</Box>
+			)}
+
+			{!Boolean(photo) && (
+				<>
+					<input
+						accept="image/*"
+						id="document-uploader"
+						type="file"
+						hidden
+						onChange={handlePhotoChange}
+						ref={fileInputRef}
+					/>
+
+					<IconButton onClick={() => fileInputRef.current?.click()}>
+						<UploadIcon />
+					</IconButton>
+				</>
+			)}
+		</Box>
+	);
+};
