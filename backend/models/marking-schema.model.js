4740f5a7e21d32a8e6d720a36d0064c79432214b
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-06-03 16:13:27 +0530

    virtual fixes

--- backend/models/marking-schema.model.js
+++ backend/models/marking-schema.model.js
@@ -1,15 +1,8 @@
 import mongoose from "mongoose";
 
-const _schema = mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-});
-
-_schema.virtual(
-	"markingSchemaAreas",
+const _schema = mongoose.Schema(
 	{
-		ref: "MarkingSchemaArea",
-		localField: "_id",
-		foreignField: "markingSchemaId",
+		name: { type: String, required: true, unique: true },
 	},
 	{
 		toJSON: {
@@ -17,5 +10,11 @@ _schema.virtual(
 		},
 	}
 );
+
+_schema.virtual("markingSchemaAreas", {
+	ref: "MarkingSchemaArea",
+	localField: "_id",
+	foreignField: "markingSchemaId",
+});
 
 export const MarkingSchema = mongoose.model("MarkingSchema", _schema);
