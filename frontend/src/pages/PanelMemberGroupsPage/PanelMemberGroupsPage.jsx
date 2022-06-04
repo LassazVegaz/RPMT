e75925b8677ec04804449ac45d06b8fc789d6cd9
oshii98 <jkaveendyaoshadhi@gmail.com>
2022-06-04 14:56:14 +0530

    PM: groups page

--- frontend/src/pages/PanelMemberGroupsPage/PanelMemberGroupsPage.jsx
+++ frontend/src/pages/PanelMemberGroupsPage/PanelMemberGroupsPage.jsx
@@ -1,5 +1,47 @@
-import { Container } from "@mui/material";
+import { Group } from "@mui/icons-material";
+import {
+	Container,
+	List,
+	ListItemAvatar,
+	ListItemButton,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 
 export const PanelMemberGroupsPage = () => {
-	return <Container maxWidth="lg">Panel member groups</Container>;
+	return (
+		<Container
+			maxWidth="lg"
+			sx={{
+				my: 5,
+			}}
+		>
+			<Typography variant="h4">Groups Assigned to You</Typography>
+
+			<List
+				sx={{
+					mt: 8,
+				}}
+			>
+				<ListItemButton>
+					<ListItemAvatar>
+						<Group />
+					</ListItemAvatar>
+					<ListItemText primary="Title" secondary="Sub text" />
+				</ListItemButton>
+				<ListItemButton>
+					<ListItemAvatar>
+						<Group />
+					</ListItemAvatar>
+					<ListItemText primary="Title" secondary="Sub text" />
+				</ListItemButton>
+				<ListItemButton>
+					<ListItemAvatar>
+						<Group />
+					</ListItemAvatar>
+					<ListItemText primary="Title" secondary="Sub text" />
+				</ListItemButton>
+			</List>
+		</Container>
+	);
 };
