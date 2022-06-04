216e4820292b6c5ed6632669eb94d86511890579
oshii98 <jkaveendyaoshadhi@gmail.com>
2022-06-04 14:56:14 +0530

    PM: groups page wired

--- frontend/src/pages/PanelMemberGroupsPage/PanelMemberGroupsPage.jsx
+++ frontend/src/pages/PanelMemberGroupsPage/PanelMemberGroupsPage.jsx
@@ -7,13 +7,29 @@ import {
 	ListItemText,
 	Typography,
 } from "@mui/material";
+import { useEffect, useState } from "react";
+import { usePanelMembers } from "../../hooks/panel-member.hook";
 
 export const PanelMemberGroupsPage = () => {
+	const { getAssignedGroups } = usePanelMembers();
+	const [groups, setGroups] = useState([]);
+
+	useEffect(() => {
+		loadData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const loadData = async () => {
+		const _groups = await getAssignedGroups();
+		setGroups(_groups);
+	};
+
 	return (
 		<Container
 			maxWidth="lg"
 			sx={{
 				my: 5,
+				minHeight: "56.2vh",
 			}}
 		>
 			<Typography variant="h4">Groups Assigned to You</Typography>
@@ -23,24 +39,17 @@ export const PanelMemberGroupsPage = () => {
 					mt: 8,
 				}}
 			>
-				<ListItemButton>
-					<ListItemAvatar>
-						<Group />
-					</ListItemAvatar>
-					<ListItemText primary="Title" secondary="Sub text" />
-				</ListItemButton>
-				<ListItemButton>
-					<ListItemAvatar>
-						<Group />
-					</ListItemAvatar>
-					<ListItemText primary="Title" secondary="Sub text" />
-				</ListItemButton>
-				<ListItemButton>
-					<ListItemAvatar>
-						<Group />
-					</ListItemAvatar>
-					<ListItemText primary="Title" secondary="Sub text" />
-				</ListItemButton>
+				{groups.map((g) => (
+					<ListItemButton key={g.id}>
+						<ListItemAvatar>
+							<Group />
+						</ListItemAvatar>
+						<ListItemText
+							primary={g.name}
+							secondary={g.project?.topic ?? "No project yet"}
+						/>
+					</ListItemButton>
+				))}
 			</List>
 		</Container>
 	);
