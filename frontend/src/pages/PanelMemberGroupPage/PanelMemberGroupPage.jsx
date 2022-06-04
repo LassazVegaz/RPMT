181f26ed3e1da3745e49ae3ad5f664a546ba33a9
oshii98 <jkaveendyaoshadhi@gmail.com>
2022-06-04 14:56:13 +0530

    panel member routing

---
+++ frontend/src/pages/PanelMemberGroupPage/PanelMemberGroupPage.jsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export const PanelMemberGroupPage = () => {
+	return <Container maxWidth="lg">Panel member single group</Container>;
+};
---
+++ frontend/src/pages/PanelMemberGroupsPage/PanelMemberGroupsPage.jsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export const PanelMemberGroupsPage = () => {
+	return <Container maxWidth="lg">Panel member groups</Container>;
+};
