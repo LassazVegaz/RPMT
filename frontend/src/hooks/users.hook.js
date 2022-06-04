cdc3c12f866089ed4ba16c97bf99ad9a5c9d9893
oshii98 <jkaveendyaoshadhi@gmail.com>
2022-06-04 14:56:14 +0530

    common profile update

--- frontend/src/hooks/users.hook.js
+++ frontend/src/hooks/users.hook.js
@@ -4,9 +4,13 @@ import { studentsHelper } from "../helpers/students.helper";
 import { supervisorHelpers } from "../helpers/supervisors.helper";
 import { panelMemberHelpers } from "../helpers/panel-member.helper";
 import { USER_ROLES } from "../constants/user-roles.constants";
+import { authHelper } from "../helpers/auth.helper";
+import { authActions } from "../redux/slices/auth.slice";
+import { useDispatch } from "react-redux";
 
 export const useUsers = () => {
 	const { callApi } = useApi();
+	const dispatch = useDispatch();
 
 	const createAccount = async (user) => {
 		try {
@@ -21,6 +25,31 @@ export const useUsers = () => {
 				else if (user.role === USER_ROLES.PANEL_MEMBER)
 					await panelMemberHelpers.createPanelMember(user);
 			});
+			return true;
+		} catch (error) {
+			return false;
+		}
+	};
+
+	const updateUser = async (id, user) => {
+		try {
+			await callApi(
+				async () => {
+					if (
+						user.role === USER_ROLES.CO_SUPERVISOR ||
+						user.role === USER_ROLES.SUPERVISOR
+					)
+						await supervisorHelpers.updateSupervisor(id, user);
+					else if (user.role === USER_ROLES.PANEL_MEMBER)
+						await panelMemberHelpers.updatePanelMember(user);
+
+					const _user = await authHelper.getLoggedInUser();
+					dispatch(authActions.setLoggedUser(_user));
+				},
+				{
+					successMessage: "User updated successfully",
+				}
+			);
 			return true;
 		} catch (error) {
 			return false;
@@ -43,5 +72,6 @@ export const useUsers = () => {
 	return {
 		isEmailAvailable,
 		createAccount,
+		updateUser,
 	};
 };
