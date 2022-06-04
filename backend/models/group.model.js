c223593670898aeae7e48dff4316a82b403a57b7
oshii98 <jkaveendyaoshadhi@gmail.com>
2022-06-04 14:56:13 +0530

    panel member to group

--- backend/models/group.model.js
+++ backend/models/group.model.js
@@ -1,33 +1,43 @@
 import mongoose from "mongoose";
 
 const _schema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    projectId: {
-      type: mongoose.Schema.Types.ObjectId,
-    },
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		projectId: {
+			type: mongoose.Schema.Types.ObjectId,
+		},
+		panelMemberId: {
+			type: mongoose.Schema.Types.ObjectId,
+		},
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
 );
 
 _schema.virtual("students", {
-  ref: "Student",
-  localField: "_id",
-  foreignField: "groupId",
+	ref: "Student",
+	localField: "_id",
+	foreignField: "groupId",
 });
 
 _schema.virtual("project", {
-  ref: "Project",
-  localField: "projectId",
-  foreignField: "_id",
-  justOne: true,
+	ref: "Project",
+	localField: "projectId",
+	foreignField: "_id",
+	justOne: true,
+});
+
+_schema.virtual("panelMember", {
+	ref: "StaffMember",
+	localField: "panelMemberId",
+	foreignField: "_id",
+	justOne: true,
 });
 
 export const Group = mongoose.model("Group", _schema);
